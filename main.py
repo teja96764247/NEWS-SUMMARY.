dcc1765e9b183d8507466efab311ee1496ffc840
DALAYAI TEJA <107502862+teja96764247@users.noreply.github.com>
2024-02-11 01:42:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+from newspaper import Article
+import nltk
+from textblob import TextBlob
+import streamlit as st
+
+st.markdown("<h2 style='text-align: center;'>NEWS SUMMARY</h2>", unsafe_allow_html=True)
+
+nltk.download('punkt')
+
+
+
+url = st.text_input("Enter URL:")
+
+if url:
+    article=Article(url)
+
+    article.download()
+    article.parse()
+
+    article.nlp()
+
+    st.markdown("<h3 style='font-weight:bold'>Tittle Of the Article</h3>", unsafe_allow_html=True)
+    st.write(article.title)
+
+
+
+    st.markdown("<h3 style='font-weight:bold'>Authors Of the Article</h3>", unsafe_allow_html=True)
+    st.write(article.authors)
+
+
+
+    st.markdown("<h3 style='font-weight:bold'>Publish Date Of the Article</h3>", unsafe_allow_html=True)
+    st.write(article.publish_date)
+
+
+    st.markdown("<h3 style='font-weight:bold'>Summary Of the Article</h3>", unsafe_allow_html=True)
+    st.write(article.summary)
+
+
+
